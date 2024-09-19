d81da0321ef6e275015155714d89fc26ddc7d967
Developerganesh01 <137500512+Developerganesh01@users.noreply.github.com>
2024-09-20 01:25:58 +0530

    bug fix and fixes

--- src/pages/SensorList.jsx
+++ src/pages/SensorList.jsx
@@ -6,40 +6,39 @@ import Sensor from '../components/Sensor';
 export default function SensorList()
 {
   const params=useParams();
-  // console.log(params);
   const[sensorlist,setSensorList]=useState([]);
-  // const[error,setError]=useState(false);
-  // const[loading,setloading]=useState(true);
   useEffect(function(){
     async function getlist(){
       try{
-        //params.type will give type
         let list=null;
         switch(params.type)
         {
-          //'Nutrient dosing ','grow light ','Environment'
-          case 'Nutrient dosing':list=await import('../data/nutrientMonitoringSensors.json');
-                                 break;
-          case 'grow light':list=await import('../data/growLightMonitoringSensors.json');
-                                 break;
-          case 'Environment':list=await import('../data/environmentMonitoringSensors.json');
-                                 break;
-          default:throw new Error('invalid url');
+          case 'Nutrient-dosing':
+            list=await import('../data/nutrientMonitoringSensors.json');
+            break;
+          case 'grow-light':
+            list=await import('../data/growLightMonitoringSensors.json');
+            break;
+          case 'Environment':
+            list=await import('../data/environmentMonitoringSensors.json');
+            break;
+          default:
+            throw new Error('invalid url');
         }
         if(!list)
         {
           throw new Error('something went wrong try again ');
         }
-        // console.log(list);
-        // console.log(typeof(list));
+        let ct=0;
        const newlist=list.default.map(item=>
-        (
-          <div className={styles.sensorcontainer}>
-            <Link to={`/${item.href}/${item.collection}`}>
-              <Sensor title={item.href}/>
+       {
+        ct=ct+1;
+         return(<div className={styles.sensorcontainer} key={ct}>
+            <Link to={`/${params.type}/${item.href}/live`}>
+              <Sensor firebaseRTDBPath={item.href} />
             </Link>
-         </div>
-       ));
+         </div>);
+      });
        setSensorList(newlist);
       }
       catch(err)
