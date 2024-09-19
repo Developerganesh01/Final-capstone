ec79ea5418cb3ed67182334dcc95f99a9d3167e3
Developerganesh01 <137500512+Developerganesh01@users.noreply.github.com>
2024-09-20 01:24:38 +0530

    Update route

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,40 @@
 import {React} from "react";
-import {BrowserRouter,Routes,Route,Link} from "react-router-dom";
+import {BrowserRouter,Routes,Route,NavLink} from "react-router-dom";
 import Homepage from './pages/Homepage';
 import About from './pages/About';
 import Contact from './pages/Contact';
 import styles from './App.module.css';
 import SensorList from './pages/SensorList';
 import Sensorgraph from './pages/Sensorgraph';
-import db from "./firebase";
 
 function App() {
+  const getClassName=function(isActive)
+  {
+    if(isActive)
+    {
+      return styles["active-btn"];
+    }
+    else
+    {
+      return "";
+    }
+  }
   return (
     <>
     <BrowserRouter>
     <header>
       <nav className={styles.navheader}>
-        <Link to="/" >Homepage</Link>
-        <Link to="/about">About</Link>
-        <Link to="/contact">Contact</Link>
+        <NavLink to="/" className={({isActive})=>{return getClassName(isActive);}}>Homepage</NavLink>
+        <NavLink to="/about" className={({isActive})=>{return getClassName(isActive);}}>About</NavLink>
+        <NavLink to="/contact" className={({isActive})=>{return getClassName(isActive);}}>Contact</NavLink>
       </nav>
     </header>
     <Routes>
       <Route path="/" element={<Homepage/>}/>
       <Route path="/about" element={<About/>} />
       <Route path="/contact" element={<Contact />}/>
-      <Route path="/:type"  element={<SensorList db={db}/>}/>
-      <Route path="/:type/:sensor" db={db} element={<Sensorgraph db={db}/>} />
+      <Route path="/:type"  element={<SensorList />}/>
+      <Route path="/:type/:sensor/*"  element={<Sensorgraph/>} />
     </Routes>
     </BrowserRouter>
     </>
